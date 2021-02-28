7cb5a1c19e454b1a91671b69d192d45d731c8960
Danilo Brignone <dbrignon@e3r8p4.42roma.it>
2021-02-28 15:58:04 +0100

    prima prova di come stampare i pixel

---
+++ test.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrignon <dbrignon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/27 14:29:22 by dbrignon          #+#    #+#             */
+/*   Updated: 2021/02/28 11:56:19 by dbrignon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+typedef struct  s_data {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_data;
+
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int             main(void)
+{
+    void    *mlx;
+    void    *mlx_win;
+    t_data  img;
+    int x;
+    int y;
+    
+    x = 100;
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    img.img = mlx_new_image(mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+    while (x != 200)
+    {
+        y = 100;
+        while (y++ != 200)
+            my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
+        x++;
+    }
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_loop(mlx);
+}
